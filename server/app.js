462c69ba7b5ad6e5d3a4d6396b0f4ed5fbbf7534
kapsi <kapsi@o2.pl>
2015-03-19 19:17:26 +0100

    update for openshift

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
-var env = process.env.NODE_ENV || 'development';
-
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 var fs = require('fs');
 var express = require('express');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var app = express();
-var port = process.env.PORT || 3000;
-var db = process.env.MONGOHQ_URL || 'mongodb://localhost/fooddiary-dev';
+var config = require('./config');
 
 app.use(compression());
 app.use(bodyParser.json());
@@ -15,12 +14,11 @@ require('./routes')(app);
 
 // Connect to mongodb
 var connect = function () {
-    var options = { server: { socketOptions: { keepAlive: 1 } } };
-    mongoose.connect(db, options);
+    mongoose.connect(config.mongo.uri, config.mongo.options);
 };
 mongoose.connection.on('error', console.log);
 mongoose.connection.on('disconnected', connect);
 
 connect();
-app.listen(port);
-console.log('Express app started on port ' + port);
+app.listen(config.port);
+console.log('Express app started on port ' + config.port);
